3fd6fb70a6487a17ea7a5ee3825bae716edee4fb
Will McGinnis <willmcg4132@gmail.com>
2025-03-12 21:59:59 -0400

    GA and dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,10 +41,15 @@ except ImportError:
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
+# Add any Sphinx extension modules here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc", 
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_dark_mode",
+    "sphinxcontrib.googleanalytics",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -61,6 +66,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+
+# Default to dark theme
+default_dark_mode = True
+
+# Google Analytics configuration
+googleanalytics_id = "G-Z43R9PWW0B"
+googleanalytics_enabled = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
